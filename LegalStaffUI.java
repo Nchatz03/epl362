fcc06a40a4dc3cd0012e06c7444ffedb88e5c362
GeorgeStavrou <32328561+GeorgeStavrou@users.noreply.github.com>
2018-04-12 17:36:36 +0300

    Add files via upload

---
+++ LegalStaffUI.java
@@ -0,0 +1,47 @@
+package com.javaworld.sample.helloservice;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public interface LegalStaffUI {
+
+	
+	public boolean getClient(ArrayList<Object[]> Q); //ok
+	
+	public boolean isCaseTheSame(String CaseTrans,int ClientID); //ok
+
+	public boolean changeCase(String CaseTrans,int ClientID); //ok
+
+	public boolean isRecommedationTheSame(String Recommendation,int ClientID);//ok
+
+	public boolean changeRecommendation(String Recommedation,int ClientID);//ok
+	
+	public boolean viewLegalStrategies(ArrayList<Object[]> Q);
+	
+	public boolean updateLater(int ClientID); // ok
+	
+	public boolean getNeedsUpdate(ArrayList<Object[]> Q);//ok
+	
+	public boolean getIllegal(ArrayList<Object[]> Q);//ok
+	
+	public boolean addDispute(int ClientID, String Date, String Description);//ok
+	
+	public boolean addDisputeTransaction(int ClientID, String Date, String Description, String LegalStrategy);
+	
+	public boolean getDispute(ArrayList<Object[]> Q);//ok
+	
+	public boolean getDisputeTransaction(ArrayList<Object[]> Q);
+	
+	public boolean getUnwilling(ArrayList<Object[]> Q); //ok
+	
+	public boolean isUnwilling(int ClientID, String LegalStrategy);//ok
+	
+	public boolean addUnwilling(int ClientID, String LegalStrategy);//ok
+	
+	
+	
+
+
+}
