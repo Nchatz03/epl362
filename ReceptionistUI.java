ba7815ec60a71a95f0ebdb6f6d7bd755a97896bf
GeorgeStavrou <32328561+GeorgeStavrou@users.noreply.github.com>
2018-04-27 00:30:22 +0300

    Add files via upload

--- ReceptionistUI.java
+++ ReceptionistUI.java
@@ -1,10 +1,10 @@
-package dbconnection;
+package epl362_project;
 
 import java.util.ArrayList;
 
 public interface ReceptionistUI {
 
-	public boolean insertClient(String Name, String Surname, String Sex,String DOB, String Telephone, String Address, boolean Legal);
+	public boolean insertClient(String Name, String Surname, String Sex, String DOB, String Telephone, String Address,String Status);
 
 	public boolean getClient(ArrayList<Object[]> Q);
 	
@@ -16,9 +16,20 @@ public interface ReceptionistUI {
 
 	public boolean checkIfClientWent(int AppointmentID, String accomplish);
 	
-	public boolean insertDropIN(int ClientID);
+	public boolean insertDropIN(int ClientID, String Date, int LawyerID, int BranchID);
 	
+	public boolean getDropIn(ArrayList<Object[]> Q);
 	
 	public boolean getLawyers(ArrayList<Object[]> Q);
+	
+	
+	public boolean insertLegalOpinion(String Rec_LegalOp); // na mpei sto management
+	
+	public boolean insertRecommendation(String Rec);  // na mpei sto management
+	
+	
+	
+	
+
 
 }
