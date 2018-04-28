ac2d6f8ea9222469c12f63baac4aabe5202614f6
Nchatz03 <chatzigiannisnick@gmail.com>
2018-04-28 15:14:55 +0300

    Add files via upload

---
+++ JunitTest/HOManagementUITest.java
@@ -0,0 +1,73 @@
+package JunitTest;
+
+import static org.junit.Assert.assertTrue;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import org.junit.Test;
+
+public class HOManagementUITest {
+
+	@Test
+	public final void testGetNotAttended() {
+		try {
+			HOManagementUI a = new HOManagementUIImpl();
+			ArrayList<Object[]> Q = new ArrayList<Object[]>();
+			String date = "test";
+			assertTrue(a.getNotAttended(Q, date));
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public final void testGetClient() {
+		try {
+			HOManagementUI a = new HOManagementUIImpl();
+			ArrayList<Object[]> Q = new ArrayList<Object[]>();
+			assertTrue(a.getClient(Q));
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public final void testInsertLegalOpinion() {
+		try {
+			HOManagementUI a = new HOManagementUIImpl();
+			String Rec_LegalOp = "test";
+			assertTrue(a.insertLegalOpinion(Rec_LegalOp));
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public final void testInsertRecommendation() {
+		try {
+			HOManagementUI a = new HOManagementUIImpl();
+			String Rec = "test";
+			assertTrue(a.insertRecommendation(Rec));
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public final void testInsertCaseType() {
+		try {
+			HOManagementUI a = new HOManagementUIImpl();
+			String CaseType = "test";
+			assertTrue(a.insertCaseType(CaseType));
+		} catch (SQLException e) {
+			
+			e.printStackTrace();
+		}
+	}
+
+}
